53a4788ae5683ab7081839b2c801d4c8a2b22dd5
zhangzhou <836300659@qq.com>
2024-05-18 11:15:10 +0800

    初始化项目

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+        'subject-full-stop': [0, 'never'],
+        'subject-case': [0, 'never'],
+    },
 };
